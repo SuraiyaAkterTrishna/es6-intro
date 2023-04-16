313311b202948068e618898be40363455f84aae3
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-16 17:00:23 +0600

    template-string.js

---
+++ template-string.js
@@ -0,0 +1,5 @@
+const multiline = "There are some thing that"+
+"I need to write in a single line"+
+"but for the testing purpose I am writing in multiline"+
+"to make muliline in single line";
+console.log(multiline);
