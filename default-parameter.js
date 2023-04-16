742ee9b0657f3132221a6f090339db3bd0186b8f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-16 16:41:29 +0600

    default parameter

---
+++ default-parameter.js
@@ -0,0 +1,5 @@
+function sum(first, second = 10){
+    return first + second;
+}
+const total = sum(10, 1);
+console.log(total);
--- dom-manipulation-recap.js
+++ dom-manipulation-recap.js
@@ -1,3 +1,20 @@
 document.getElementById('apply-bg').addEventListener('click', function(){
-    console.log('apply bg button clicked');
-})
+    const players = document.getElementsByClassName('player');
+    for (const player of players) {
+        player.style.backgroundColor = 'lightblue';
+    }
+});
+document.getElementById('apply-center').addEventListener('click', function(){
+    const third = document.getElementById('third');
+    third.style.textAlign = 'center';
+});
+document.getElementById('add-new').addEventListener('click', function(){
+    const playerContainer = document.getElementById('players');
+    const newPlayer = document.createElement('div');
+    newPlayer.classList.add('player');
+    newPlayer.innerHTML = `
+        <h3>New Player</h3>
+        <p>New another player created by js dom</p>
+    `;
+    playerContainer.appendChild(newPlayer);
+});
