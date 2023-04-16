5225a1a02870505785bb38eb26ecbe5f59a66c1a
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-16 17:07:03 +0600

    result

--- template-string.js
+++ template-string.js
@@ -8,4 +8,12 @@ const multiline = "There are some thing that \n"+
 const newMultiLine = `I am very happy 
 because now I can write multiline without 
 any problem just with the backtik`;
-console.log(newMultiLine);
+// console.log(newMultiLine);
+
+const a = 20; 
+const b = 30;
+const result = "The sum of: " + a + " and " + b + " is: " + (a+b);
+// console.log(result);
+
+const newResult = `The sum of ${a} and ${b} is: ${a+b}`;
+console.log(newResult);
