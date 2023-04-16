681f1e09ca5e19a392e0af9eb0fb910392cdfef5
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-16 17:03:45 +0600

    multiline with template string

--- template-string.js
+++ template-string.js
@@ -1,5 +1,11 @@
-const multiline = "There are some thing that"+
-"I need to write in a single line"+
-"but for the testing purpose I am writing in multiline"+
+const multiline = "There are some thing that \n"+
+"I need to write in a single line \n"+
+"but for the testing purpose I am writing in multiline\n"+
 "to make muliline in single line";
-console.log(multiline);
+// console.log(multiline);
+
+// multiline with backtik
+const newMultiLine = `I am very happy 
+because now I can write multiline without 
+any problem just with the backtik`;
+console.log(newMultiLine);
