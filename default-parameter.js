ef702441602eae6005ca053adbdb29e05dc7ec26
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-16 16:44:30 +0600

    default parameter for string

--- default-parameter.js
+++ default-parameter.js
@@ -3,3 +3,9 @@ function sum(first, second = 10){
 }
 const total = sum(10, 1);
 console.log(total);
+
+function fullName(firstName, lastName = 'Hassan'){
+    return firstName + ' ' + lastName;
+}
+const myName = fullName('Rukaiyah', 'Islam');
+console.log(myName);
